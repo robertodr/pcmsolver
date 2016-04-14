6e4258a5ade7288c41c91c749a8cca9ef347410e
Roberto Di Remigio <roberto.d.remigio@uit.no>
2016-04-14 16:06:11 +0200

    Added a  classical charge distribution struct
    Only monopoles and point dipoles for the moment

--- src/green/IGreensFunction.hpp
+++ src/green/IGreensFunction.hpp
@@ -2,22 +2,22 @@
 /*
  *     PCMSolver, an API for the Polarizable Continuum Model
  *     Copyright (C) 2013-2016 Roberto Di Remigio, Luca Frediani and contributors
- *     
+ *
  *     This file is part of PCMSolver.
- *     
+ *
  *     PCMSolver is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU Lesser General Public License as published by
  *     the Free Software Foundation, either version 3 of the License, or
  *     (at your option) any later version.
- *     
+ *
  *     PCMSolver is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *     GNU Lesser General Public License for more details.
- *     
+ *
  *     You should have received a copy of the GNU Lesser General Public License
  *     along with PCMSolver.  If not, see <http://www.gnu.org/licenses/>.
- *     
+ *
  *     For information on the complete list of contributors to the
  *     PCMSolver API, see: <http://pcmsolver.readthedocs.org/>
  */
@@ -55,8 +55,15 @@ typedef pcm::function<double(const Eigen::Vector3d &, const Eigen::Vector3d &)> 
 
 /*! \typedef KernelD
  *  \brief functor handle to the kernelD method
+ *  \note This is the conormal derivative
  */
 typedef pcm::function<double(const Eigen::Vector3d &, const Eigen::Vector3d &, const Eigen::Vector3d &)> KernelD;
+
+/*! \typedef DerivativeProbe
+ *  \brief functor handle to the derivativeProbe method
+ *  \note This is the directional derivative wrt the probe point
+ */
+typedef pcm::function<double(const Eigen::Vector3d &, const Eigen::Vector3d &, const Eigen::Vector3d &)> DerivativeProbe;
 
 class IGreensFunction
 {
@@ -87,6 +94,9 @@ class IGreensFunction
     }
     KernelD exportKernelD() const {
       return exportKernelD_impl();
+    }
+    DerivativeProbe exportDerivativeProbe() const {
+      return exportDerivativeProbe_impl();
     }
 
     /*! Whether the Green's function describes a uniform environment */
@@ -125,6 +135,7 @@ class IGreensFunction
         const Eigen::Vector3d & p1, const Eigen::Vector3d & p2) const = 0;
     virtual KernelS exportKernelS_impl() const = 0;
     virtual KernelD exportKernelD_impl() const = 0;
+    virtual DerivativeProbe exportDerivativeProbe_impl() const = 0;
     virtual std::ostream & printObject(std::ostream & os) = 0;
 };
 
---
+++ tests/utils/utils_newton-potential.cpp
@@ -0,0 +1,104 @@
+/* pcmsolver_copyright_start */
+/*
+ *     PCMSolver, an API for the Polarizable Continuum Model
+ *     Copyright (C) 2013-2015 Roberto Di Remigio, Luca Frediani and contributors
+ *
+ *     This file is part of PCMSolver.
+ *
+ *     PCMSolver is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     PCMSolver is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with PCMSolver.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *     For information on the complete list of contributors to the
+ *     PCMSolver API, see: <http://pcmsolver.readthedocs.org/>
+ */
+/* pcmsolver_copyright_end */
+
+#include "catch.hpp"
+
+#include <vector>
+#include <cmath>
+
+
+#include <Eigen/Core>
+
+#include "cavity/GePolCavity.hpp"
+#include "bi_operators/CollocationIntegrator.hpp"
+#include "green/DerivativeTypes.hpp"
+#include "green/Vacuum.hpp"
+#include "green/IonicLiquid.hpp"
+#include "green/UniformDielectric.hpp"
+#include "utils/ChargeDistribution.hpp"
+#include "TestingMolecules.hpp"
+
+SCENARIO("Calculation of the Newton potential", "[utils][newton_potential][utils_newton-potential]")
+{
+  GIVEN("A classical charge distribution of monopoles")
+  {
+    double area = 0.4;
+    double probeRadius = 0.0;
+    double minRadius = 100.0;
+    Molecule molec = C2H4();
+    GePolCavity cavity = GePolCavity(molec, area, probeRadius, minRadius, "newton");
+    ChargeDistribution dist = nuclearChargeDistribution(molec);
+
+    WHEN("the Newton potential is calculated in vacuum")
+    {
+      Vacuum<> gf;
+      Eigen::VectorXd newton = computeNewtonPotential(gf.exportKernelS(),
+          cavity.elementCenter(), dist);
+      THEN("comparison with the computeMEP method results in")
+      {
+        Eigen::VectorXd mep = computeMEP(molec, cavity.elements());
+        REQUIRE(newton.size() == mep.size());
+        for (int i = 0; i < mep.size(); ++i) {
+          REQUIRE(newton(i) == Approx(mep(i)));
+        }
+      }
+    }
+
+    AND_WHEN("the Newton potential is calculated in a uniform dielectric")
+    {
+      double permittivity = 78.39;
+      UniformDielectric<> gf(permittivity);
+      Eigen::VectorXd newton = computeNewtonPotential(gf.exportKernelS(),
+          cavity.elementCenter(), dist);
+      THEN("comparison with the computeMEP method results in")
+      {
+        Eigen::VectorXd mep = computeMEP(molec, cavity.elements());
+        mep /= permittivity;
+        REQUIRE(newton.size() == mep.size());
+        for (int i = 0; i < mep.size(); ++i) {
+          REQUIRE(newton(i) == Approx(mep(i)));
+        }
+      }
+    }
+
+    AND_WHEN("the Newton potential is calculated in an ionic liquid with kappa = 0.0")
+    {
+      double permittivity = 78.39;
+      double kappa = 0.0;
+      IonicLiquid<> gf(permittivity, kappa);
+      Eigen::VectorXd newton = computeNewtonPotential(gf.exportKernelS(),
+          cavity.elementCenter(), dist);
+      THEN("comparison with the computeMEP method results in")
+      {
+        Eigen::VectorXd mep = computeMEP(molec, cavity.elements());
+        mep /= permittivity;
+        REQUIRE(newton.size() == mep.size());
+        for (int i = 0; i < mep.size(); ++i) {
+          REQUIRE(newton(i) == Approx(mep(i)));
+        }
+      }
+    }
+  }
+}
